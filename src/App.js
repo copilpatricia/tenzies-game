9a90b97bd71ec900a10e968d24fab8f2ef8bac98
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-06 21:26:37 -0500

    add the nanoid library to generate random ids

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Die from "./components/Die";
 import {useState} from "react";
+//generate a random id
+import { nanoid } from 'nanoid'
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
   // valueDice will be an array of Die elements with the value props set to the same number as the value of the index inside the array
   const valueDice = dice.map((num) => {
-    return <Die value={num}/>;
+    return <Die value={num.value} key={num.id}/>;
   });
   function allNewDice() {
     let arr = [];
     for (let i = 0; i < 10; i++) {
       let value = Math.floor(Math.random() * 7) + 1;
-      arr.push(value);
+      arr.push({value: value, isHeld: false, id: nanoid()});
     }
     return arr;
   }
