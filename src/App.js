8b32cab633cb4e5364ae40e1c3bc6ac6fcc5deab
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-06 19:09:19 -0500

    button roll created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
+  // valueDice will be an array of Die elements with the value props set to the same number as the value of the index inside the array
   const valueDice = dice.map((num) => {
     return <Die value={num}/>;
   });
@@ -21,6 +22,7 @@ function App() {
       <div className="board">
         <div className="container-die">
             {valueDice}
+        <button id="btn-roll">Roll</button>
         </div>
       </div>
     </div>
