9f2239288aea2e1d66d2c23765113b08ba2e6a48
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-08 16:46:30 -0500

    update the state of the array and change the isHeld property when it is clicked

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Die from "./components/Die";
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  // generate random number between 1-7
   function allNewDice() {
     let arr = [];
     for (let i = 0; i < 10; i++) {
@@ -14,19 +15,38 @@ function App() {
     }
     return arr;
   }
-  allNewDice();
-
+ 
+  // generate new numbers everytime we push the roll button
   function toggleRoll() {
     setDice(allNewDice());
   }
 
+  // update the state of the array using the setDice function
+  // we need the previous value of the array to determine the new array
+  //return a new array - only the isHeld property will change
+  //if the id is the same with the one passed into the function - update the array, otherwise return the same die
   function handleDice(id) {
-    console.log(id)
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        if (die.id === id) {
+          return { ...die, isHeld: !die.isHeld };
+        } else {
+          return die;
+        }
+      })
+    );
   }
 
   // valueDice will be an array of Die elements with the value props set to the same number as the value of the index inside the array
   const valueDice = dice.map((num) => {
-    return <Die key={num.id} value={num.value} isHeld={num.isHeld} handleDice={() => handleDice(num.id)} />;
+    return (
+      <Die
+        key={num.id}
+        value={num.value}
+        isHeld={num.isHeld}
+        handleDice={() => handleDice(num.id)}
+      />
+    );
   });
 
   return (
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,9 @@
 function Die(props) {
+    //ternary operator - render based on the isHeld property - true/false
     let color = props.isHeld ? "#8eb1c7" : "rgb(234, 200, 114)"
+
   return (
+    // pass a function to the child component using props
     <div onClick={props.handleDice} >
       <h2 className="num" style={{backgroundColor: color}}>{props.value}</h2>
     </div>
